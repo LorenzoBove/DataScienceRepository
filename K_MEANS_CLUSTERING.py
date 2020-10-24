9ddd00c94176df7e5f07d192f75c046303886879
LorenzoBove <56440797+LorenzoBove@users.noreply.github.com>
2020-10-24 16:11:02 +0200

    Update K_MEANS_CLUSTERING.py

--- K_MEANS_CLUSTERING.py
+++ K_MEANS_CLUSTERING.py
@@ -4,8 +4,8 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 
 #Per determinare il valore di k possiamo utilizzare l'elbow method,
-# che ci dice che il valore da scegliere è quello in cui si osserva il ' \
-#cambiamento di velocità nel grafico
+# che ci dice che il valore da scegliere è quello in cui si osserva il ' \ cambiamento di velocità nel grafico
+
 
 def KMEANSclusteringfunction(X):
     sse = {}
@@ -17,12 +17,13 @@ def KMEANSclusteringfunction(X):
 
     for k in range(1, 10):
         kmeans = KMeans(init="k-means++", n_clusters=k).fit(X)
-        #print(data["clusters"])
-        sse[k] = kmeans.inertia_ # Inertia: Sum of distances of samples to their closest cluster center
+        
+        sse[k] = kmeans.inertia_ 
+        # Inertia: Sum of distances of samples to their closest cluster center
     plt.figure()
     plt.plot(list(sse.keys()), list(sse.values()),marker='o')
     plt.xlabel("Number of clusters", fontsize=16)
-    #somma delle distance al quadrato
+    
     plt.ylabel("Squared sum of distances ", fontsize=16)
     plt.savefig("number_of_k.png")
     plt.show()
